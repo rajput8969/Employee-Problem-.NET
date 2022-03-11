0255ff495ba7cbacf58b14928b51bbe6ecea3fe5
ANMOL <anmol999rajput@gmail.com>
2022-03-11 17:03:29 +0530

    Adding UC-5

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -1,4 +1,4 @@
-﻿//Solving using Switch Case Statement//
+﻿//Calculating Wages For a Month assuming 20 Working Days in a Month//
 using System;
 
 namespace Employee_Problem_.NET
@@ -8,28 +8,33 @@ namespace Employee_Problem_.NET
         public const int IS_Part_Time = 1;
         public const int IS_Full_Time = 2;
         public const int Emp_Rate_Per_hour = 20;
+        public const int Num_Of_Working_Days = 2;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmWage = 0;
+            for (int day = 0; day < Num_Of_Working_Days; day++)
             {
-                case IS_Part_Time:
-                    empHrs = 4;
-                    break;
-                case IS_Full_Time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-             }
-            empWage = empHrs * Emp_Rate_Per_hour;
-            Console.WriteLine("Employment Wage :-" + empWage);
+              Random random = new Random();
+              int empCheck = random.Next(0, 3);
+              switch (empCheck)
+              {
+                  case IS_Part_Time:
+                      empHrs = 4;
+                      break;
+                  case IS_Full_Time:
+                       empHrs = 8;
+                        break;
+                   default:
+                         empHrs = 0;
+                          break;
+              }
+              empWage = empHrs * Emp_Rate_Per_hour;
+              totalEmWage += empWage;
+              Console.WriteLine("Emp Wage :-" + empWage);
+            }
+            Console.WriteLine("Total Emp Wage :- " + totalEmWage);          
         }
     }
 }
