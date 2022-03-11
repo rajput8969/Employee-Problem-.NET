a9185d601adf4b5d4d08557e932b0558899148cf
ANMOL <anmol999rajput@gmail.com>
2022-03-11 16:35:40 +0530

    Adding UC-2

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -1,4 +1,4 @@
-﻿       //Adding Part Time Employee And Wages//
+﻿       //Calculate Daily Employee Wage//
 using System;
 
 namespace Employee_Problem_.NET
@@ -7,8 +7,8 @@ namespace Employee_Problem_.NET
     {
         static void Main(string[] args)
         {
-            int IS_Part_Time = 1;
-            int IS_Full_Time = 2;
+        
+            int IS_Full_Time = 1;
             int Emp_Rate_Per_hour = 20;
 
             int empHrs = 0;
@@ -16,12 +16,8 @@ namespace Employee_Problem_.NET
 
             Random random = new Random();
 
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_Part_Time)
-            {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_Full_Time)
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_Full_Time)
             {
                 empHrs = 8;
             }
