5caffb4363eb4d717442855cddc2af32b473a385
ANMOL <anmol999rajput@gmail.com>
2022-03-11 16:46:22 +0530

    Solving Using Switch Case

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -1,34 +1,33 @@
-﻿//Adding Part Time Employee And Wages//
+﻿//Solving using Switch Case Statement//
 using System;
 
 namespace Employee_Problem_.NET
 {
     class Program
     {
+        public const int IS_Part_Time = 1;
+        public const int IS_Full_Time = 2;
+        public const int Emp_Rate_Per_hour = 20;
         static void Main(string[] args)
         {
-            int IS_Part_Time = 1;
-            int IS_Full_Time = 2;
-            int Emp_Rate_Per_hour = 20;
-
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_Part_Time)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_Full_Time)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+                case IS_Part_Time:
+                    empHrs = 4;
+                    break;
+                case IS_Full_Time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+             }
             empWage = empHrs * Emp_Rate_Per_hour;
             Console.WriteLine("Employment Wage :-" + empWage);
         }
