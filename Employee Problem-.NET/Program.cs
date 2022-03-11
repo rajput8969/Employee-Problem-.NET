6a0745b45ed2b706b5e4589bf15176df241c00bf
ANMOL <anmol999rajput@gmail.com>
2022-03-11 16:02:07 +0530

    Add project files.

---
+++ Employee Problem-.NET/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Employee_Problem_.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee !");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("    ************************      ");
+            Console.WriteLine("UC-1:-Check Employee is Present or Absent");
+            Console.WriteLine("UC-2:-Calculate daily Employee Wage");
+            Console.WriteLine("UC-3:-Adding Part time Employee and Wage");
+            Console.WriteLine("UC-4:-Solving Usng Switch case Statement");
+            Console.WriteLine("UC-5:-Calculate Wages for a Month");
+            Console.WriteLine("UC-6:-Calculate Wages till a sondition of total working Hours or days is reached for a month");
+        }
+    }
+}
