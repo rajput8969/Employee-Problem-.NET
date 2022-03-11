1c4cc2167a0beb5aea966686db19db7c0aa5c10e
ANMOL <anmol999rajput@gmail.com>
2022-03-11 16:15:12 +0530

    Adding UC-1

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -1,20 +1,24 @@
-﻿using System;
+﻿       //Check Employee is Present or Absent//
+using System;
 
 namespace Employee_Problem_.NET
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee !");
-            Console.WriteLine("******************************************");
-            Console.WriteLine("    ************************      ");
-            Console.WriteLine("UC-1:-Check Employee is Present or Absent");
-            Console.WriteLine("UC-2:-Calculate daily Employee Wage");
-            Console.WriteLine("UC-3:-Adding Part time Employee and Wage");
-            Console.WriteLine("UC-4:-Solving Usng Switch case Statement");
-            Console.WriteLine("UC-5:-Calculate Wages for a Month");
-            Console.WriteLine("UC-6:-Calculate Wages till a sondition of total working Hours or days is reached for a month");
+            int IS_Full_Time = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
