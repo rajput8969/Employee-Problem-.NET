21c1c8f503cffcac2393f610a644c2d0cbcbe432
ANMOL <anmol999rajput@gmail.com>
2022-03-11 16:23:30 +0530

    Adding Employee Wages

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -8,17 +8,24 @@ namespace Employee_Problem_.NET
         static void Main(string[] args)
         {
             int IS_Full_Time = 1;
+            int Emp_Rate_Per_hour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_Full_Time)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * Emp_Rate_Per_hour;
+            Console.WriteLine("Employment Wage :-" + empWage);
         }
     }
 }
