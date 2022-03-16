c568b468fe0cf9652725c1c47981a7d944837893
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-03-16 15:58:02 +0530

    Update Program.cs

--- Employee Problem-.NET/Program.cs
+++ Employee Problem-.NET/Program.cs
@@ -1,42 +1,70 @@
-﻿//Calculating Wages till a condition of Total Working Hours of 100 or max days and 20 is reached for a month //
+            // UC-9 Ability to save the Total Wage for Each Company //
 using System;
 
-namespace Employee_Problem_.NET
+namespace UC_9
 {
     class Program
     {
-        public const int IS_Part_Time = 1;
-        public const int IS_Full_Time = 2;
-        public const int Emp_Rate_Per_hour = 20;
-        public const int Num_Of_Working_Days = 2;
-        public const int Max_Hrs_In_Months = 10;
         static void Main(string[] args)
         {
-            int empHrs = 0;
-            int totalWorkingDays = 0;
-            int totalEmpHrs = 0;
-            while (totalEmpHrs <= Max_Hrs_In_Months && totalWorkingDays < Num_Of_Working_Days)
-            {
-              totalWorkingDays++;
-              Random random = new Random();
-              int empCheck = random.Next(0, 3);
-              switch (empCheck)
-              {
-                  case IS_Part_Time:
-                      empHrs = 4;
-                      break;
-                  case IS_Full_Time:
-                       empHrs = 8;
-                        break;
-                   default:
-                         empHrs = 0;
-                          break;
-              }
-              totalEmpHrs += empHrs;
-              Console.WriteLine("Days#:-" + totalWorkingDays + " Emp Hrs :-" + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * Emp_Rate_Per_hour;
-            Console.WriteLine("Total Emp Wage :- " + totalEmpWage);          
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeWage();
+            Console.Write(reliance.toString());
         }
     }
+    public class EmpWageBuilderObject
+    {
+        public const int IS_PartTime = 1;
+        public const int IS_FullTime = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHourssPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHours;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHourssPerMonth = maxHoursPerMonth;
+        }
+        public void computeWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs <= this.maxHourssPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PartTime:
+                        empHrs = 4;
+                        break;
+                    case IS_FullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:-" + totalWorkingDays + " Emp Hrs :-" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company :- " + company + "is:" + totalEmpWage);
+
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :- " + this.company + "is:" + this.totalEmpWage;
+        }
+    }
+    
 }
